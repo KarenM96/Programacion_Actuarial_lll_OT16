b610f4e39ad3ecd7039489d4b5b6fe438586c51c
Karen Marin <karen_301196@hotmail.com>
2016-09-26 10:09:47 -0500

    Caso 1

--- clase nacho.R
+++ clase nacho.R
@@ -466,6 +466,43 @@ paste("a","b",sep = ":")
 paste("a","b",se=":")
 args(cat)
 
+lm
+lm<-function(x){x*x}
+lm
+rm(lm)
+lm
 
+search()
+library(stats)
+search()
 
-  
+#funciones dentro de otras fuciones
+hacer.potencia<-function(n){
+  potencia<-function(x){
+    x^n
+  }
+  potencia
+}  
+
+cubica<-hacer.potencia(3)
+cuadrada<-hacer.potencia(2)
+
+cubica(3)
+cuadrada(3)
+
+#entorno de una funcion
+ls(environment(cubica))
+get("n",environment(cubica))
+ls(environment(cuadrada))
+get("n",environment(cuadrada))
+
+#ejemplo
+y<-10
+f<-function(x){
+  y<-2
+  y^2+g(x)
+}
+g<-function(x){
+  x*y
+}
+f(3)
